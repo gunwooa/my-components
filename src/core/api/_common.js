3786d5f6280f2a13a6bc4706e460274abf6616b8
gw.jang <gw.jang@cardoc.co.kr>
2021-01-10 01:45:18 +0900

    ..

--- src/core/api/_common.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_HOST,
-  timeout: 120000,
-  withCredentials: true,
-});
--- src/core/api/tire.js
+++
@@ -1,8 +0,0 @@
-import { api } from './_common';
-import { stringify } from 'query-string';
-
-export const getTireList = (params = {}) => {
-  const queryString = stringify(params);
-  const endpoint = `/api/list?mode=list&${queryString}`;
-  return api.get(endpoint);
-};
